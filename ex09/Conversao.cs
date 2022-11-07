c3ed7b201fad770f8df6c68618b1cda5f43a6cd1
Joao Pedro Soares <jpsoares111@hotmail.com>
2022-11-07 06:31:13 -0300

    ex bem rapidin so p fixar

---
+++ ex09/Conversao.cs
@@ -0,0 +1,10 @@
+namespace ContacaoDolar
+{
+	public class Conversao
+	{
+		static double IOF = 1.06;
+
+
+		public static double Dolar(double dolar, double quant) => dolar*quant*IOF;
+	}
+}
---
+++ ex09/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ContacaoDolar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Qual é a cotacao do dolar hoje? ");
+			double dolar = Double.Parse(Console.ReadLine());
+			Console.Write("Quantos dolares voce vai comprar? ");
+			double qt = Double.Parse(Console.ReadLine());
+			Console.WriteLine($"Valor a ser pago em reais = {Conversao.Dolar(dolar,qt)}");
+		}
+	}
+}
