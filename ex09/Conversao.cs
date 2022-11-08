d1466ba554d0c61f3447617795d8f4e76fa8059a
Joao Pedro Soares <jpsoares111@hotmail.com>
2022-11-08 06:41:44 -0300

    contrutores/usando sem

--- ex09/Conversao.cs
+++ ex09/Conversao.cs
@@ -3,8 +3,6 @@ namespace ContacaoDolar
 	public class Conversao
 	{
 		static double IOF = 1.06;
-
-
 		public static double Dolar(double dolar, double quant) => dolar*quant*IOF;
 	}
 }
---
+++ ex10/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Product
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Produto p = new Produto()
+			{
+				Nome = "TV",
+				Preco = 500.00,
+				Quantidade = 20
+			};
+			Console.WriteLine(p);
+		}
+	}
+}
+
+
+
+//Usando costrutores
+
+
+
+// namespace Product
+// {
+// 	class Program
+// 	{
+// 		static void Main(string[] args)
+// 		{
+// 			Produto p = new Produto("Tv", 500.00, 4);
+// 			Console.WriteLine(p);
+// 		}
+// 	}
+// }
